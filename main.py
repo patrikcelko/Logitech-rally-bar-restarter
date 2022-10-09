ae505cc900275b4527e6a964fd9671cb59b82db6
Patrik Čelko <xcelko@fi.muni.cz>
2022-10-10 00:22:01 +0200

    Few bugfixes, add option for IP

--- main.py
+++ main.py
@@ -3,29 +3,31 @@ from selenium.webdriver.common.by import By
 
 
 class WebManipulator:
-
-    LOGIN_URL = 'https://10.210.11.104/login'
-    RESTART_URL = 'https://10.210.11.104/settings/about'
+    LOGIN_URL = 'https://{}/login'
+    RESTART_URL = 'https://{}/settings/about'
     USERNAME_INPUT = '// *[ @ id = "username"]'
     PASSWORD_INPUT = '// * [ @ id = "password"]'
     LOGIN_BUTTON = '/html/body/div[1]/div[1]/div[1]/div/form/div[6]/button'
     EXPANDER_BUTTON = '/html/body/div[1]/main/div[1]/div[2]/div/div/div[5]/button/div'
     RESTART_BUTTON = '/html/body/div[1]/main/div[1]/div[2]/div/div/div[5]/div/div/div/div[1]/button'
-    CONFIRM_BUTTON = ''  # TODO: XPath for Confirm button
+    CONFIRM_BUTTON = '/html/body/div[1]/main/div[1]/div[2]/div/div/div[5]/div/div/div/div[1]/div/div[2]/div[2]/div[' \
+                     '3]/button[1] '
 
     # This is just to ensure we are on the correct website
     TITLE_SHOULD_CONTAIN = 'CollabOS'
 
     # Well sometimes it takes a while to load a page, so let's give it time
-    WAIT_TIME = 4  # In seconds
+    WAIT_TIME = 5  # In seconds
 
-    def __init__(self, username, password, dry_run=False):
+    def __init__(self, username, password, ip, dry_run=False):
         self.username = username
+        self.ip = ip
         self.password = password
         self.dry_run = dry_run
 
         # In dry-run, we hide the browser (use a headless one)
         options = webdriver.ChromeOptions()
+        options.add_argument('ignore-certificate-errors')
         if not self.dry_run:
             options.add_argument('headless')
             # This is just problems with wrongly set meta-state for the website
@@ -37,9 +39,9 @@ class WebManipulator:
         self.driver.close()
 
     def _login(self):
-        self.driver.get(self.LOGIN_URL)
+        self.driver.get(self.LOGIN_URL.format(self.ip))
         self.driver.implicitly_wait(self.WAIT_TIME)
-        assert(self.TITLE_SHOULD_CONTAIN in self.driver.title)
+        assert (self.TITLE_SHOULD_CONTAIN in self.driver.title)
 
         # Filling inputs with credentials
         self.driver.find_element(By.XPATH, self.USERNAME_INPUT).send_keys(self.username)
@@ -49,24 +51,30 @@ class WebManipulator:
         self.driver.find_element(By.XPATH, self.LOGIN_BUTTON).click()
 
     def _restart(self):
-        self.driver.get(self.RESTART_URL)
+        self.driver.get(self.RESTART_URL.format(self.ip))
         self.driver.implicitly_wait(self.WAIT_TIME)
         assert (self.TITLE_SHOULD_CONTAIN in self.driver.title)
 
         # This is probably unnecessary because dropdown usually is just JS think and elements are already there
         self.driver.find_element(By.XPATH, self.EXPANDER_BUTTON).click()
+        self.driver.implicitly_wait(self.WAIT_TIME)
         self.driver.find_element(By.XPATH, self.RESTART_BUTTON).click()
         self.driver.implicitly_wait(self.WAIT_TIME)  # Yes... let's wait
-        if not self.dry_run
+        if not self.dry_run:
             self.driver.find_element(By.XPATH, self.CONFIRM_BUTTON).click()
 
     def execute(self, disable_kill=False):
         self._login()
+        while 'login' in self.driver.current_url:
+            pass  # We will be waiting for the page to load
+
         self._restart()
+        self.driver.implicitly_wait(self.WAIT_TIME)
 
         if not disable_kill:
             self._kill()
 
 
-manipulator = WebManipulator(username='', password='')
-manipulator.execute()
+# Only part which you should modify :)
+manipulator = WebManipulator(username='', password='', ip='', dry_run=False)
+manipulator.execute()  # Let's run the scraper
